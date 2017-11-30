36a256947057ad385f46dcdb24b5e37bbf9d0b73
Adam Bramley <adam.bramley@previousnext.com.au>
2017-11-30 16:35:05 +1100

    All the CLI flags

--- main.go
+++ main.go
@@ -1,55 +1,57 @@
 package main
 
 import (
-	"github.com/parnurzeal/gorequest"
 	"fmt"
 	"os"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/parnurzeal/gorequest"
 )
 
+var (
+	cliStatus      = kingpin.Flag("status", "Status").Required().String()
+	cliKey         = kingpin.Flag("key", "Key").Required().String()
+	cliName        = kingpin.Flag("name", "Name").Required().String()
+	cliUrl         = kingpin.Flag("url", "Url").Required().String()
+	cliDescription = kingpin.Flag("description", "Description").Required().String()
+	cliUsername    = kingpin.Flag("username", "Username").Required().Envar("BITBUCKET_STATUS_USERNAME").String()
+	cliPassword    = kingpin.Flag("password", "Password").Required().Envar("BITBUCKET_STATUS_PASSWORD").String()
+	cliCommitHash  = kingpin.Flag("hash", "Commit hash").Required().Envar("BITBUCKET_COMMIT").String()
+	cliRepoOwner   = kingpin.Flag("owner", "Repo owner").Required().Envar("BITBUCKET_REPO_OWNER").String()
+	cliRepoSlug    = kingpin.Flag("slug", "Repo slug").Required().Envar("BITBUCKET_REPO_SLUG").String()
+)
+
+const baseUrl = "https://api.bitbucket.org/2.0/repositories"
+
 func main() {
+	kingpin.Parse()
+
 	status := Status{
-		State: StateSuccessful,
-		Key: "test123",
-		Name: "Build environment",
-		Url: "https://test.com",
-		Description: "My cool build",
+		State:       *cliStatus,
+		Key:         *cliKey,
+		Name:        *cliName,
+		Url:         *cliUrl,
+		Description: *cliDescription,
 	}
-	request := gorequest.New().SetBasicAuth("pnx_nick", "NkZmtJcDV6Ha5rJbgews")
-	_, body, errs := request.Post("https://api.bitbucket.org/2.0/repositories/transportfornsw/tfnsw-corp/commit/88b252aa80e56fef7cdbc0a776e711c84e5d87dd/statuses/build").
+
+	request := gorequest.New().SetBasicAuth(*cliUsername, *cliPassword)
+	url := fmt.Sprintf("%s/%s/%s/commit/%s/statuses/build", baseUrl, *cliRepoOwner, *cliRepoSlug, *cliCommitHash)
+	_, body, errs := request.Post(url).
 		Send(status).
 		End()
+
 	fmt.Println(body)
-	for _, err := range errs  {
+	for _, err := range errs {
 		fmt.Println(err)
 	}
+
 	if len(errs) > 0 {
 		os.Exit(1)
 	}
 }
 
-// "pnx_nick", "8dkAAPt9vBbLUb6UCYJG"
-// Commit hash 88b252aa80e56fef7cdbc0a776e711c84e5d87dd
-// Base URL: https://bitbucket.org/transportfornsw/
-// https://<bitbucket-base-url>/rest/build-status/1.0/commits/<commit-hash>
-//{
-//	"state": "<INPROGRESS|SUCCESSFUL|FAILED>",
-//	"key": "<build-key>",
-//	"name": "<build-name>",
-//	"url": "<build-url>",
-//	"description": "<build-description>"
-//}
-
-type State string
-
-const (
-	StateInProgress State = "INPROGRESS"
-	StateSuccessful State = "SUCCESSFUL"
-	StateFailed 	State = "FAILED"
-	StateStopped	State = "STOPPED"
-)
-
 type Status struct {
-	State       State  `json:"state"`
+	State       string `json:"state"`
 	Key         string `json:"key"`
 	Name        string `json:"name"`
 	Url         string `json:"url"`
